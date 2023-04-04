f47a4a09faa8231992f2444733e605c7435799b2
toriumi <syouei72@icloud.com>
2023-04-05 03:01:36 +0900

    wip

--- nuxt-playground/nuxt.config.ts
+++ nuxt-playground/nuxt.config.ts
@@ -6,4 +6,17 @@ export default defineNuxtConfig({
   nitro: {
     preset: "firebase",
   },
+  runtimeConfig: {
+    public: {
+      apiKey: "AIzaSyDnB7-4BA_3lY-3hVVO2hDRCDkx0nVXico",
+      authDomain: "nuxt-playground-d783b.firebaseapp.com",
+      projectId: "nuxt-playground-d783b",
+      storageBucket: "nuxt-playground-d783b.appspot.com",
+      messagingSenderId: "218892116934",
+      appId: "1:218892116934:web:924471127b61bea8c6c8c7",
+      measurementId: "G-2WCRRYNXSH",
+      channelSecret:"133dd7e73c12249624f89eaf8c8295a1",
+      channelAccessToken:"eyJhbGciOiJIUzI1NiJ9.VizSaGFBq7mC-2TNJY8K33tG3cv0Mb1qtRGlyfMo8ScPowbGlbcKagWRHoBDqHm7XrOgTfdMqkxaKaSTyRx8VbPCXtv6TJ7WB4C5KznLRF-jOC05aWnTxuVCrA5G70fP.uhzTWUj53GfCIX68HjgPuzSp4uJQAEiz48ybpKEn_Ew"
+    }
+  }
 })
---
+++ nuxt-playground/server/routes/lineWebhook.ts
@@ -0,0 +1,155 @@
+// import { WebhookRequestBody, Client } from '@line/bot-sdk'
+
+// // 実行時に必要なパラメータを環境変数から取得
+// const config = useRuntimeConfig()
+// const channel = {
+//   channelSecret: config.public.channelSecret,
+//   channelAccessToken: config.public.channelAccessToken
+// }
+
+// if (!channel.channelSecret || !channel.channelSecret) {
+//   throw new Error('Environment variables for CHANNEL_SECRET and CHANNEL_ACCESS_TOKEN must be set')
+// }
+
+// export default defineEventHandler(async (event) => {
+//   if (event.node.req.method !== 'POST') {
+//     event.res.writeHead(405, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'Only POST method is allowed' }))
+//     return
+//   }
+
+//   // LINE Messaging API Clientの初期化
+//   const lineClient = new Client({
+//     channelSecret: channel.channelSecret,
+//     channelAccessToken: channel.channelAccessToken
+//   })
+
+//   // ユーザーがbotに送ったメッセージをそのまま返す
+//   const body = JSON.parse(await readBody(event))
+//   const events = (body as WebhookRequestBody).events
+//   events.forEach((event) => {
+//     switch (event.type) {
+//       case 'message': {
+//         const { replyToken, message } = event
+//         if (message.type === 'text') {
+//           lineClient.replyMessage(replyToken, { type: 'text', text: message.text })
+//         }
+//         break
+//       }
+//       default:
+//         break
+//     }
+//   })
+
+//   try {
+//     event.res.writeHead(200, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'HTTP POST request sent to the webhook URL!' }))
+//   } catch (error) {
+//     console.error(error)
+//     event.res.writeHead(500, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'Internal Server Error' }))
+//   }
+// })
+
+
+// import { WebhookRequestBody, Client } from '@line/bot-sdk'
+
+// // 実行時に必要なパラメータを環境変数から取得
+// const config = useRuntimeConfig()
+// const channel = {
+//   channelSecret: config.public.channelSecret,
+//   channelAccessToken: config.public.channelAccessToken
+// }
+
+// if (!channel.channelSecret || !channel.channelSecret) {
+//   throw new Error('Environment variables for CHANNEL_SECRET and CHANNEL_ACCESS_TOKEN must be set')
+// }
+
+// async function handleMessage(events:any) {
+//   // LINE Messaging API Clientの初期化
+//   const lineClient = new Client({
+//     channelSecret: channel.channelSecret,
+//     channelAccessToken: channel.channelAccessToken
+//   })
+
+//   for (const event of events) {
+//     switch (event.type) {
+//       case 'message': {
+//         const { replyToken, message } = event
+//         if (message.type === 'text') {
+//           await lineClient.replyMessage(replyToken, { type: 'text', text: message.text })
+//         }
+//         break
+//       }
+//       default:
+//         break
+//     }
+//   }
+// }
+
+// export default defineEventHandler(async (event) => {
+//   if (event.node.req.method !== 'POST') {
+//     event.res.writeHead(405, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'Only POST method is allowed' }))
+//     return
+//   }
+
+//   try {
+//     const body = JSON.parse(await readBody(event))
+//     const events = (body as WebhookRequestBody).events
+
+//     // メッセージの処理を別の非同期関数で行い、完了を待つ
+//     await handleMessage(events)
+
+//     event.res.writeHead(200, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'HTTP POST request sent to the webhook URL!' }))
+//   } catch (error) {
+//     console.error(error)
+//     event.res.writeHead(500, { 'Content-Type': 'application/json' })
+//     event.res.end(JSON.stringify({ message: 'Internal Server Error' }))
+//   }
+// })
+
+import { WebhookRequestBody, Client } from "@line/bot-sdk";
+
+// 実行時に必要なパラメータを環境変数から取得
+const config = useRuntimeConfig();
+const channel = {
+  channelSecret: config.public.channelSecret,
+  channelAccessToken: config.public.channelAccessToken,
+};
+
+export default defineEventHandler(async (event) => {
+  if (event.node.req.method !== "POST") {
+    event.res.writeHead(405, { "Content-Type": "application/json" });
+    event.res.end(JSON.stringify({ message: "Only POST method is allowed" }));
+    return;
+  }
+
+  event.res.writeHead(200, { "Content-Type": "application/json" });
+  event.res.end(JSON.stringify({ message: "HTTP POST request sent to the webhook URL!" }));
+
+  // LINE Messaging API Clientの初期化
+  const lineClient = new Client({
+    channelSecret: channel.channelSecret,
+    channelAccessToken: channel.channelAccessToken,
+  });
+
+  // ユーザーがbotに送ったメッセージをそのまま返す
+  const body = JSON.parse(await readBody(event));
+  const events = (body as WebhookRequestBody).events;
+  events.forEach((event) => {
+    switch (event.type) {
+      case "message": {
+        const { replyToken, message } = event;
+        if (message.type === "text") {
+          lineClient.replyMessage(replyToken, { type: "text", text: message.text });
+        }
+
+        break;
+      }
+      default:
+        break;
+    }
+  });
+});
