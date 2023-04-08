f7a85fcca81a0f614338d8696b2c788118bf59fa
toriumi <syouei72@icloud.com>
2023-04-09 05:18:11 +0900

    wip

--- nuxt-playground/server/api/generate.post.ts
+++ nuxt-playground/server/api/generate.post.ts
@@ -1,39 +1,65 @@
 import { Configuration, OpenAIApi, CreateChatCompletionRequest } from 'openai'
 
-const generatePrompt = (animal: string) => `
-${animal}
-`.trim()
+// const generatePrompt = (animal: string) => `
+// ${animal}
+// `.trim()
+
+
+// export default defineEventHandler(async (event) => {
+//   const { openaiApiKey: apiKey } = useRuntimeConfig().public
+//   const { animal = '' } = await readBody(event)
+
+//   if (!animal) {
+//     return event.node.res.end('No animal provided')
+//   }
+
+//   const configuration = new Configuration({ apiKey })
+//   const openai = new OpenAIApi(configuration)
+
+//   // OpenAI config
+//   const model = 'text-davinci-003'
+//   const prompt = generatePrompt(animal)
+//   const temperature = 0.5
+
+//   const completion = await openai.createCompletion({
+//     model,
+//     prompt,
+//     temperature,
+//     max_tokens: 2048,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0
+//   })
+
+  
+
+//   const result = completion.data.choices[0].text?.trim() || ''
+
+//   return {
+//     result,
+//   }
+// })
 
 
 export default defineEventHandler(async (event) => {
   const { openaiApiKey: apiKey } = useRuntimeConfig().public
   const { animal = '' } = await readBody(event)
 
-  if (!animal) {
-    return event.node.res.end('No animal provided')
-  }
-
-  const configuration = new Configuration({ apiKey })
-  const openai = new OpenAIApi(configuration)
-
-  // OpenAI config
-  const model = 'text-davinci-003'
-  const prompt = generatePrompt(animal)
-  const temperature = 0.5
-
-  const completion = await openai.createCompletion({
-    model,
-    prompt,
-    temperature,
-    max_tokens: 2048,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0
+  const configuration = new Configuration({
+    apiKey: apiKey,
   })
-
-  const result = completion.data.choices[0].text?.trim() || ''
+  const openai = new OpenAIApi(configuration)
+  
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 2048,
+    messages: [{ role: "user", content: `${animal}` }],
+  });
+  console.log(completion.data.choices[0].message)
+  const result = completion.data.choices[0].message?.content || ''
 
   return {
     result,
   }
 })
+
--- nuxt-playground/server/routes/lineWebhook.post.ts
+++ nuxt-playground/server/routes/lineWebhook.post.ts
@@ -1,205 +1,3 @@
-// import { WebhookRequestBody, Client } from '@line/bot-sdk'
-
-// // 実行時に必要なパラメータを環境変数から取得
-// const config = useRuntimeConfig()
-// const channel = {
-//   channelSecret: config.public.channelSecret,
-//   channelAccessToken: config.public.channelAccessToken
-// }
-
-// if (!channel.channelSecret || !channel.channelSecret) {
-//   throw new Error('Environment variables for CHANNEL_SECRET and CHANNEL_ACCESS_TOKEN must be set')
-// }
-
-// export default defineEventHandler(async (event) => {
-//   if (event.node.req.method !== 'POST') {
-//     event.res.writeHead(405, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'Only POST method is allowed' }))
-//     return
-//   }
-
-//   // LINE Messaging API Clientの初期化
-//   const lineClient = new Client({
-//     channelSecret: channel.channelSecret,
-//     channelAccessToken: channel.channelAccessToken
-//   })
-
-//   // ユーザーがbotに送ったメッセージをそのまま返す
-//   const body = JSON.parse(await readBody(event))
-//   const events = (body as WebhookRequestBody).events
-//   events.forEach((event) => {
-//     switch (event.type) {
-//       case 'message': {
-//         const { replyToken, message } = event
-//         if (message.type === 'text') {
-//           lineClient.replyMessage(replyToken, { type: 'text', text: message.text })
-//         }
-//         break
-//       }
-//       default:
-//         break
-//     }
-//   })
-
-//   try {
-//     event.res.writeHead(200, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'HTTP POST request sent to the webhook URL!' }))
-//   } catch (error) {
-//     console.error(error)
-//     event.res.writeHead(500, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'Internal Server Error' }))
-//   }
-// })
-
-
-// import { WebhookRequestBody, Client } from '@line/bot-sdk'
-
-// // 実行時に必要なパラメータを環境変数から取得
-// const config = useRuntimeConfig()
-// const channel = {
-//   channelSecret: config.public.channelSecret,
-//   channelAccessToken: config.public.channelAccessToken
-// }
-
-// if (!channel.channelSecret || !channel.channelSecret) {
-//   throw new Error('Environment variables for CHANNEL_SECRET and CHANNEL_ACCESS_TOKEN must be set')
-// }
-
-// async function handleMessage(events:any) {
-//   // LINE Messaging API Clientの初期化
-//   const lineClient = new Client({
-//     channelSecret: channel.channelSecret,
-//     channelAccessToken: channel.channelAccessToken
-//   })
-
-//   for (const event of events) {
-//     switch (event.type) {
-//       case 'message': {
-//         const { replyToken, message } = event
-//         if (message.type === 'text') {
-//           await lineClient.replyMessage(replyToken, { type: 'text', text: message.text })
-//         }
-//         break
-//       }
-//       default:
-//         break
-//     }
-//   }
-// }
-
-// export default defineEventHandler(async (event) => {
-//   if (event.node.req.method !== 'POST') {
-//     event.res.writeHead(405, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'Only POST method is allowed' }))
-//     return
-//   }
-
-//   try {
-//     const body = JSON.parse(await readBody(event))
-//     const events = (body as WebhookRequestBody).events
-
-//     // メッセージの処理を別の非同期関数で行い、完了を待つ
-//     await handleMessage(events)
-
-//     event.res.writeHead(200, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'HTTP POST request sent to the webhook URL!' }))
-//   } catch (error) {
-//     console.error(error)
-//     event.res.writeHead(500, { 'Content-Type': 'application/json' })
-//     event.res.end(JSON.stringify({ message: 'Internal Server Error' }))
-//   }
-// })
-
-// import { WebhookRequestBody, Client } from "@line/bot-sdk";
-
-// // 実行時に必要なパラメータを環境変数から取得
-// const config = useRuntimeConfig();
-// const channel = {
-//   channelSecret: config.public.channelSecret,
-//   channelAccessToken: config.public.channelAccessToken,
-// };
-
-
-// export default defineEventHandler(async (event) => {
-//   // const { test } = useAuth()
-//   if (event.node.req.method !== "POST") {
-//     event.res.writeHead(405, { "Content-Type": "application/json" });
-//     // event.res.end(JSON.stringify({ message: "Only POST method is allowed" }));
-//     return;
-//   }
-//   // test.value = "test"
-//   event.res.writeHead(200, { "Content-Type": "application/json" });
-//   // event.res.end(JSON.stringify({ message: "HTTP POST request sent to the webhook URL!" }));
-
-//   // LINE Messaging API Clientの初期化
-//   const lineClient = new Client({
-//     channelSecret: channel.channelSecret,
-//     channelAccessToken: channel.channelAccessToken,
-//   });
-
-//   // ユーザーがbotに送ったメッセージをそのまま返す
-//   const body = JSON.parse(await readBody(event));
-//   const events = (body as WebhookRequestBody).events;
-//   events.forEach((event) => {
-//     switch (event.type) {
-//       case "message": {
-//         const { replyToken, message } = event;
-//         if (message.type === "text") {
-//           lineClient.replyMessage(replyToken, { type: "text", text: message.text });
-//         }
-
-//         break;
-//       }
-//       default:
-//         break;
-//     }
-//   });
-// });
-
-// import { WebhookRequestBody, Client } from "@line/bot-sdk" 
-// import { Configuration, OpenAIApi } from 'openai'
-
-// export default defineEventHandler((event:any) => {
-
-//   event.node.res.sendStatus(200)
-
-//   const { channelSecret, channelAccessToken } = useRuntimeConfig().private
-
-//   const lineClient = new Client({
-//     channelSecret: channelSecret,
-//     channelAccessToken: channelAccessToken,
-//   })
-
-//   const { events } = event.node.req.body as WebhookRequestBody
-
-//   events.forEach((event) => {
-
-//     if (event.type === 'message') {
-
-//       const { replyToken, message } = event
-
-//       if (message.type === 'text') {
-
-//         switch (message.text) {
-//         case "入室しました": {
-//           lineClient.replyMessage(replyToken, {type: "text", text: "入室時の金額を入力"})
-//           break
-//         }
-//         case "退出しました": {
-//           lineClient.replyMessage(replyToken, {type: "text", text: "お疲れ様でした"})
-//           break
-//         }
-//         default:
-//           break
-//         }
-//       }
-//     }
-//   })
-
-//   event.node.res.end()
-
-// })
-
 import { WebhookRequestBody, Client } from '@line/bot-sdk';
 import { Configuration, OpenAIApi } from 'openai';
 
@@ -210,23 +8,27 @@ const openaiConfig = new Configuration({
   apiKey: openaiApiKey,
 });
 
-const chatDataByUser = {}
+const chatData:any = [];
 
-async function generateResponse(prompt: string) {
+async function generateResponse(message: string) {
   const openaiInstance = new OpenAIApi(openaiConfig);
 
-  const response = await openaiInstance.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0.7,
-    max_tokens: 506,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0
+  const messages = chatData.map((messageObj: { role: string; content: string }) => messageObj);
+
+  messages.push(
+    {role: "system", content: "あなたは、優秀な執事です。相手はお姫様です。"},
+    {role: "system", content: "会話の相手のことは、お嬢と呼んでください。"},
+    {role: "system", content: "あなたは、お嬢のことが大好きで、愛が溢れてしまい、毎回好きと伝えます。"},
+    {role: "user", content: message},
+  );
+
+  const completion = await openaiInstance.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 180,
+    messages: messages,
   });
 
-  return response.data.choices[0].text?.trim() || '';
-
+  return completion.data.choices[0].message?.content || '';
 }
 
 export default defineEventHandler((event: any) => {
@@ -255,16 +57,12 @@ export default defineEventHandler((event: any) => {
             break;
           }
           default: {
-            // OpenAI API を使用して応答を生成
-            // replyText = '何で？'
-            const prompt = `
-            ${message.text}
-            `.trim();
-            replyText = await generateResponse(prompt);
-
+            replyText = await generateResponse(message.text);
             break;
           }
         }
+
+        chatData.push({ role: "user", content: replyText });
 
         lineClient.replyMessage(replyToken, { type: 'text', text: replyText });
       }
@@ -273,5 +71,6 @@ export default defineEventHandler((event: any) => {
 
   event.node.res.end();
 });
+
 
 
